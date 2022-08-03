9b032dabd0fbcbce3e4649db734d8f623d135ebe
Brian Denmark <testingarea479@gmail.com>
2022-08-02 21:18:28 -0700

    added 'add odd integers'

--- file.py
+++ file.py
@@ -8,3 +8,20 @@ for x in range(5,1001):
         print(x)
 
 # Counting, the Dojo Way
+for x in range(1,101):
+    if(x % 5 == 0 and x % 10 == 0):
+        print("Coding Dojo")
+    elif(x % 5 == 0):
+        print("Coding")
+    else:
+        print(x)
+
+# Add odd integers from 0 to 500,00 and print the final sum
+sum = 0
+
+for x in range(1,500001):
+    if(x % 2 != 0):
+        # print(f"{x} is odd")
+        sum = sum + x
+
+print(sum)
