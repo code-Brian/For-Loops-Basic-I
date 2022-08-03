a6749373cf84a42f9e11c85db139869d03f9e2bb
Brian Denmark <testingarea479@gmail.com>
2022-08-02 20:54:22 -0700

    init commit

---
+++ file.py
@@ -0,0 +1,10 @@
+# Basic - Print all integers from 0 to 150.
+for x in range(0,151):
+    print(x)
+
+# Multiples of Five - Print all the multiples of 5 from 5 to 1,000
+for x in range(5,1001):
+    if (x % 5 ==0):
+        print(x)
+
+# Counting, the Dojo Way
